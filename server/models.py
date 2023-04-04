145a3cc9671793de6153eed2ae6a29dee422a518
triciahughes <hughes.a.tricia@gmail.com>
2023-04-04 09:15:31 -0700

    app config and user table created

--- server/models.py
+++ server/models.py
@@ -3,3 +3,32 @@ from sqlalchemy_serializer import SerializerMixin
 from config import db
 
 # Models go here!
+
+class User(db.Model, SerializerMixin):
+
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, nullable=False)
+    _password_hash = db.Column(db.String)
+
+    @hybrid_property
+    def password_hash(self):
+        return self._password_hash
+        
+    @password_hash.setter
+    def password_hash(self, password):
+        password_hash = bcrypt.generate_password_hash(
+            password.encode('utf-8')
+        )
+        self._password_hash = password_hash.decode('utf-8')
+
+    def authenticate(self, password):
+        return bcrypt.check_password_hash(
+            self._password_hash, password.encode('utf-8')
+        )
+
+    def __repr__(self):
+        return f'<Username: {self.username} Password: {self._password_hash}'
+
+    
