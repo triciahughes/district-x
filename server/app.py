6846021dd7b08b33d68662b785db5cb079418f9b
Tricia Hughes <116763138+triciahughes@users.noreply.github.com>
2023-04-04 17:49:26 -0700

    backend views for signin / signup / authorized user created

--- server/app.py
+++ server/app.py
@@ -21,7 +21,75 @@ def index(id=0):
 # Local imports
 from config import app, db, api
 
-# Views go here!
+# Views!
+class Signup(Resource):
+
+    def post(self):
+
+        data = request.get_json()
+
+        new_user = User(
+
+            username=data['username']
+        )
+
+        new_user.password_hash = data['password']
+
+        
+        db.session.add(new_user)
+        db.session.commit()
+
+        session['user_id'] = new_user.id
+
+        response = make_response(
+            new_user.to_dict(),
+            201
+        )
+        return response
+
+class AuthorizedSession(Resource):
+
+    def get(self):
+
+        user = User.query.filter(User.id == session.get('user_id')).first()
+
+        if user:
+
+            response = make_response(
+                jsonify(user.to_dict()),
+                200
+            )
+            return response
+        
+        print("Did not find user.")
+
+        return {'error': '401 Unauthorized'}, 401
+
+class Signin(Resource):
+    def post(self):
+
+        data = request.get_json()
+
+        username = data.get('username')
+        password = data.get('password')
+
+        user = User.query.filter(User.username == username).first()
+
+            
+        if user.authenticate(data['password']):
+
+            session['user_id'] = user.id
+
+            response = make_response(
+                user.to_dict(),
+                200
+            )
+            return response
+        return {'error' : "Invalid Username or Password"}, 401
+
+api.add_resource(Signup, '/signup')
+api.add_resource(AuthorizedSession, '/authorized')
+api.add_resource(Login, '/signin')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
