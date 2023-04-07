5e0dee61c4a47c3eb26e688f342b80abfab52655
Tricia Hughes <116763138+triciahughes@users.noreply.github.com>
2023-04-06 19:51:16 -0700

    cleanup comments and reseed data

--- client/src/components/Posts.js
+++ client/src/components/Posts.js
@@ -44,64 +44,8 @@ function Posts({ user, fetchPost, id, upvotes, downvotes, posts, postUser }) {
     console.log("Clicked");
   }
 
-  // const list = posts.map((post) => {
-  //   const downvotes = post.downvotes;
-  //   return (
-  //     <Box
-  //       sx={{
-  //         flexGrow: 1,
-  //         overflow: "hidden",
-  //         px: 3,
-  //         backgroundColor: "white",
-  //       }}
-  //       key={post.id}
-  //     >
-  //       <StyledPaper
-  //         sx={{
-  //           my: 2,
-  //           mx: "auto",
-  //           p: 2,
-  //         }}
-  //       >
-  //         <Grid container wrap="nowrap" spacing={2}>
-  //           <Grid item>
-  //             <Avatar>L</Avatar>
-  //           </Grid>
-  //           <Grid item xs key={post.id} onClick={handlePostClick}>
-  //             <Typography fontWeight={600}>
-  //               <Link href="" underline="hover" color="orange">
-  //                 {post.user.username}
-  //               </Link>
-  //             </Typography>
-  //             <Typography fontWeight={100}>{post.post}</Typography>
-  //           </Grid>
-  //           <ButtonGroup>
-  //             <List>
-  //               <ListItem>
-  //                 <ListItemButton onClick={handleUpvoteClick}>
-  //                   <ArrowUpwardIcon></ArrowUpwardIcon>
-  //                 </ListItemButton>
-  //                 <p style={{ color: "#03a9f4" }}>{upvotes}</p>
-  //               </ListItem>
-  //             </List>
-  //             <List>
-  //               <ListItem>
-  //                 <ListItemButton onClick={handleDownvoteClick}>
-  //                   <ArrowDownwardIcon></ArrowDownwardIcon>
-  //                 </ListItemButton>
-  //                 <p style={{ color: "#ff9100" }}>{post.downvotes}</p>
-  //               </ListItem>
-  //             </List>
-  //           </ButtonGroup>
-  //         </Grid>
-  //       </StyledPaper>
-  //     </Box>
-  //   );
-  // });
-
   return (
     <>
-      {" "}
       <Box
         sx={{
           flexGrow: 1,
